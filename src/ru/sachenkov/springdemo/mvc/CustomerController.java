7563a24485213eff2116ae374ca476734b5642d3
Vallov <oqiwhite1996@gmail.com>
2020-05-27 16:57:11 +0300

    How to Handle String input for Integer Fields - Deep Dive

--- src/ru/sachenkov/springdemo/mvc/CustomerController.java
+++ src/ru/sachenkov/springdemo/mvc/CustomerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.sql.SQLOutput;
 
 @Controller
 @RequestMapping("/customer")
@@ -29,6 +30,8 @@ public class CustomerController {
     @RequestMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
         System.out.println("Last name: |" + theCustomer.getLastName() + "|");
+        System.out.println("Binding result : " + theBindingResult);
+        System.out.println("\n\n\n\n");
         if (theBindingResult.hasErrors()) {
             return "customer-form";
         } else {
